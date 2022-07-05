385924fb7a85655dec8892582997e21002a74a78
Dorian Edwards <de.pharmd2018@gmail.com>
2022-07-05 17:15:00 -0400

    add root and first api route, install nodemon as dev dependency

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
-const express = require('express') //initializing express+its methods 
+const express = require('express') //initializing express+its methods
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+
+const PORT = 3001
+
+let notes = [
+  { user: 'Striemer#4188', country: 'DEU' },
+  { user: 'wart#0416', country: 'USA' },
+  { user: 'ISellDrugsToTheCommunity#8648', country: 'USA' },
+]
+
+// root directory
+app.get('/', (req, res) => {
+  res.send('<h1>Test</h1>')
+})
+
+// req and res are short for request and response
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
