69b0bad19b1c6de092059112e76493e53accbd81
Sebastian Triemer <sebastian.triemer@outlook.de>
2022-07-09 18:06:55 +0200

    built the userinterface, added the post routing, installed the body-parser and bootstrap-icons package

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express"); //initializing express+its methods
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const PORT = 3001;
+//middleware to access the content of req.body
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 
@@ -23,12 +25,19 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //connecting 
 
 app.set("view engine", "ejs"); //middleware
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // root directory
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+//Logs the new tasks to the console - should be changed so that the new task is added to the database
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 // req and res are short for request and response
