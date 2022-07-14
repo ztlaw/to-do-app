f08bf33d88a97f2a46baed1f74f8a9aee66fd5b3
Dorian Edwards <de.pharmd2018@gmail.com>
2022-07-13 21:36:57 -0600

    add edit function

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express') //initializing express+its methods
 const app = express()
+const morgan = require('morgan')
 const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectId
 const PORT = 3001
 //middleware to access the content of req.body
 const bodyParser = require('body-parser')
 
 require('dotenv').config()
-
-let notes = [
-  { user: 'Striemer#4188', country: 'DEU' },
-  { user: 'wart#0416', country: 'USA' },
-  { user: 'ISellDrugsToTheCommunity#8648', country: 'USA' },
-]
 
 let db,
   dbConnectionStr = process.env.DB_STRING,
@@ -21,39 +17,49 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) //connecting 
   .then((client) => {
     console.log(`Now connected to the ${dbName} database.`)
     db = client.db(dbName)
-
-    //Creates a collection named tasks
-    const tasksCollection = db.collection('tasks')
-    app.post('/', (req, res) => {
-      //adds submitted tasks to the task collection
-      tasksCollection.insertOne(req.body)
-      res.redirect('/')
-    })
-
-    app.get('/', (req, res) => {
-      //reads the tasks from the database
-      db.collection('tasks')
-        .find()
-        .toArray()
-        .then((results) => {
-          res.render('index.ejs', { tasks: results })
-        })
-    })
   })
 
+app.use(morgan('dev'))
 app.set('view engine', 'ejs') //middleware
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// root directory
+app.get('/', (req, res) => {
+  //reads the tasks from the database
+  db.collection('tasks')
+    .find()
+    .toArray()
+    .then((results) => {
+      res.render('index.ejs', { tasks: results })
+    })
+})
 
-//Logs the new tasks to the console - should be changed so that the new task is added to the database
+//Creates a collection named tasks
+// de - I commented this out so we can move routes outside of the mongoDB connect function.
+// w/o this we can call our collection with db.collection('tasks')
+// const tasksCollection = db.collection('tasks')
+app.post('/', (req, res) => {
+  //adds submitted tasks to the task collection
+  db.collection('tasks')
+    .insertOne(req.body)
+    .then((data) => {
+      return res.redirect('/')
+    })
+})
 
-// req and res are short for request and response
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.put('/', (req, res) => {
+  db.collection('tasks')
+    .updateOne(
+      { _id: new ObjectId(req.body.id) },
+      { $set: { taskDescription: req.body.taskDescription } }
+    )
+    .then((data) => {
+      return res.send(
+        db.collection('tasks').findOne({ _id: new ObjectId(req.body.id) })
+      )
+    })
 })
 
 app.listen(process.env.PORT || PORT, () => {
